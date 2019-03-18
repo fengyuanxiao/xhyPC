efeb349292b7c3fc66ff2531e93e22bd8a6e2f62
fyxiao <nbtv@foxmail.com>
2019-03-18 17:24:18 +0800

    压缩xhyPC项目

---
+++ build/precache-manifest.8e77675f56e865800faa5a4c7c87e411.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "44fcd79f97838e88ac0554d82d444b57",
+    "url": "./static/media/registerImg.44fcd79f.jpg"
+  },
+  {
+    "revision": "29c4060e5c1d5a3ae7e06cfe5930a891",
+    "url": "./static/media/logo.29c4060e.png"
+  },
+  {
+    "revision": "1a64edd9f074f3da8891e60bd774bc77",
+    "url": "./static/media/login-bg.1a64edd9.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "36f35eedb40f2527df8c",
+    "url": "./static/js/main.36f35eed.chunk.js"
+  },
+  {
+    "revision": "fd70604abaae9d61b479",
+    "url": "./static/js/1.fd70604a.chunk.js"
+  },
+  {
+    "revision": "36f35eedb40f2527df8c",
+    "url": "./static/css/main.861346d8.chunk.css"
+  },
+  {
+    "revision": "fd70604abaae9d61b479",
+    "url": "./static/css/1.663e871d.chunk.css"
+  },
+  {
+    "revision": "3f325572ecb5104c7f719184f9cfa303",
+    "url": "./index.html"
+  }
+];
